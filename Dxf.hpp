d922f300779896c725ea232be19de69245672a21
devTonyWanderley <devtonywanderley@gmail.com>
2026-09-21 17:15:16 -0300

    anotação no hpp sobre o construtor

--- Dxf.hpp
+++ Dxf.hpp
@@ -5,6 +5,7 @@
 #include <limits>
 #include <vector>
 #include <filesystem>
+//#include <fstream> .. para a alteração pretendida: construtor lê o arquivo.
 
 namespace DXF {
 enum CodGrupo{STRING, DOUBLE, I16, I32, I64};
@@ -27,6 +28,7 @@ public:
 class Dxf
 {
 private:
+    //std::ifstream mArquivo(); .. O construtor deve abrir o arquivo uma vez só e fazer todas as operações
     std::vector<Fresta> GetBufBruto(const std::filesystem::path &nome);
     size_t NumDeLinhas(const std::filesystem::path &nome);
     std::uint32_t NumPorTipo(const std::filesystem::path &nome, std::uint32_t &str, std::uint32_t &dbl,
